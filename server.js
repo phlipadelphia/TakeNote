fb089a66ff24513639e8518ecbe4ed518d152a53
Zephyr <phillip.t.mock@gmail.com>
2022-04-29 15:08:36 -0700

    final commit for submittal

--- server.js
+++ server.js
@@ -62,10 +62,7 @@ app.post("/api/notes", (req, res) => {
 
     })
 
-});
-
-// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
+}); 
 
 //setuop   HTML routes
 //GET /notes` should return the `notes.html` file.
